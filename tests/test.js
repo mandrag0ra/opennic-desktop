fc59c8925c504d0bd3a58b07b8e2afb2c7c4be0f
wil <git@arcanes.re>
2018-03-20 15:02:57 +0100

    use chai promises

--- tests/test.js
+++ tests/test.js
@@ -6,7 +6,6 @@ const osCmd = require("../lib/" + process.platform);
 const expect = require("chai").expect;
 
 Chai.should();
-Chai.use(ChaiHTTP);
 
 describe("Network", () => {
   it("get current DNS configuration and should return at least an array", done => {
@@ -14,19 +13,36 @@ describe("Network", () => {
       .getCurrentDns()
       .then(data => {
         expect(data).to.be.an("array");
-        expect(data[0]).to.have.property("dns");
+        expect(data).to.have.property("dns");
         done();
       })
       .catch(err => done(err));
   });
-  it("ping opennic.org with OS binarie and should return success if connect", done => {
-    osCmd
-      .ping("opennic.org")
+  it("ping opennic.org with OS binarie and should return success if connect", () => {
+    return osCmd.ping("opennic.org").then(res => {
+      expect(res)
+        .to.be.a("string")
+        .to.equal("success");
+    });
+  });
+  it("ping a malformed host and throw an error", () => {
+    return osCmd
+      .ping("brfzfrf")
       .then(res => {
-        expect(res).to.be.a("string");
-        expect(res).to.match(/^success|down/);
-        done();
+        throw new Error("This is not supposed to succeed !");
       })
-      .catch(err => done(err));
+      .catch(err => {
+        expect(err).to.match(/.* has to be an IP or a FQDN/);
+      });
+  });
+  it("ping an IP that is not reachable and throw an error", () => {
+    return osCmd
+      .ping("1.2.3.4")
+      .then(res => {
+        throw new Error("This is not supposed to succeed !");
+      })
+      .catch(err => {
+        expect(err).to.match(/.* is not reachable/);
+      });
   });
 });
